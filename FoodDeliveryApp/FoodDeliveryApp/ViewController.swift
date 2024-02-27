2ec319a2cc492a76fd2b842f4479e80a8973934f
MorozovArtem8 <Vjhjpjaa32@yandex.ru>
2024-02-27 18:04:12 +0300

    create project

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Artem Morozov on 27.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
